884b3d70d6081dfc10bae87042acb26091aae0cf
rainstar <45524987+prefixRAINSTARsuffix@users.noreply.github.com>
2022-01-28 19:39:17 +0800

    Update feature_extractor.py

--- deep_sort/deep/feature_extractor.py
+++ deep_sort/deep/feature_extractor.py
@@ -7,7 +7,7 @@ import logging
 import sys
 # so that init does not execute in the package
 sys.path.append('deep_sort/deep/reid')
-from .reid.torchreid import models
+import .reid.torchreid.models
 
 
 class Extractor(object):
